ac99a91ec80ab2998c3f4fb664681f3230322dbc
xyroton <xyroton@users.noreply.github.com>
2025-06-02 19:43:33 +0200

    adjust side

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://xyroton.github.io",
+  site: "https://xyroton.com",
   // base: "/astro-blog",
 
   markdown: {
