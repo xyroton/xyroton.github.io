8ef039f7e8c1f55c85b0bccaa05632ed8c1fab28
xyroton <xyroton@users.noreply.github.com>
2025-06-02 17:08:56 +0200

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import rehypeKatex from "rehype-katex";
 // https://astro.build/config
 export default defineConfig({
   site: "https://xyroton.github.io",
-  base: "/astro-blog",
+  // base: "/astro-blog",
 
   markdown: {
     remarkPlugins: [remarkMath],
