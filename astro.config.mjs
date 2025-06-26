f3e5eee23f97f6690742378320f0c599ad05cf5c
xyroton <xyroton@users.noreply.github.com>
2025-06-26 23:11:35 +0200

    code block expressive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://xyroton.com",
   // base: "/astro-blog",
-
+  integrations: [
+    expressiveCode({
+      // You can optionally override the plugin's default settings here
+      frames: {
+        // Example: Hide the "Copy to clipboard" button
+        showCopyToClipboardButton: true,
+      },
+    }),
+  ],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
@@ -19,3 +27,4 @@ export default defineConfig({
     },
   },
 });
+
