c0db53f62c5ed643ce1f06e68f53cd749067a614
xyroton <xyroton@users.noreply.github.com>
2025-06-30 17:06:46 +0200

    rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Xyroton Blog',
+    description: 'A blog about programming and other things.',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id.replace('.md', '')}/`,
+    })),
+  });
+}
